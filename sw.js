905585082aa2b56b39044129ab6951ed35f66f67
glyndavies001 <glyndavies001@gmail.com>
2026-08-15 19:49:59 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "movecalc-v1.0.1";
+const CACHE = "movecalc-v1.0.2";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", e => {
